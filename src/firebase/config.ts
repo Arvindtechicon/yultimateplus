c9468f1c673559c788ccfef3c665506d0bc74d0a
Aravind S Gudi <aravindsgudi2005@gmail.com>
2025-11-02 12:43:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "v2y-ultimate-pulse-56876-52cd7",
+  "appId": "1:464139941177:web:5b0c49f60021bcc44ae4e8",
+  "apiKey": "AIzaSyCE040fHItwE4Ag7mntyHGIWyhca2NYCCw",
+  "authDomain": "v2y-ultimate-pulse-56876-52cd7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "464139941177"
+};
